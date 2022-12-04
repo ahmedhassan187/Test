0ba53086f38d174a3e5c4f3d683810debfb7ae75
AhmedHassan178 <87389435+AhmedHassan178@users.noreply.github.com>
2022-12-04 16:00:34 +0200

    edit

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
 import streamlit as st
 import time
+if 'Flag' not in st.session_state:
+    st.session_state.Flag = True
 start_time = time.time()
 end_time = start_time
 while (end_time -start_time)<99:
     end_time = time.time()
-    text = st.text("open")
+    if st.session_state.Flag:
+        text = st.text("open")
+        st.session_state.Flag = False
     
